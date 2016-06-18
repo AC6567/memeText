23dcb34e004f3992d8b28b1f3f967147123c5995
nicolrx <nicolas.leroux@sciencespo.fr>
2016-06-17 19:35:18 -0700

    meme generation
    Hit the button and get the meme

--- index.js
+++ index.js
@@ -9,3 +9,15 @@
 module.exports = function(number, locale) {
     return number.toLocaleString(locale);
 };
+
+
+/* Get the query and send a API request to memegen.link */
+
+function sendRequest() {
+	var query = document.getElementById("input-query").value;
+	console.log(query);
+	var meme = "<img src='http://memegen.link/doge/" + query + ".jpg' />";
+	var content = document.getElementById("result").innerHTML = "<p>"+meme+"</p>";
+
+}
+
